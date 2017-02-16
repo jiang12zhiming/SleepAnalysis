21f2520efeae3d15328f54ee104d653f7e36799c
Zhiming Jiang <zhimingjiang@host-124140.dhcp.egr.msu.edu>
2017-02-16 16:49:16 -0500

    let we start

---
+++ build/motionAware.build/Debug-watchsimulator/DataAccessWatch.build/Objects-normal/i386/DataAccessWatch_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/zhimingjiang/SleepAnalysis/build/motionAware.build/Debug-watchsimulator/DataAccessWatch.build/DerivedSources/DataAccessWatch_vers.c
---
+++ motionAware/testgit.swift
@@ -0,0 +1,10 @@
+//
+//  testgit.swift
+//  motionAware
+//
+//  Created by Zhiming Jiang on 2/16/17.
+//  Copyright © 2017 ZL J. All rights reserved.
+//
+
+import Foundation
+//gona test 
